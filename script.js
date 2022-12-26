322ce4103145a6170bccd4c5bec64cfc5ee87703
mamichaelis87 <mallenm1993@gmail.com>
2022-12-26 14:40:24 -0500

    Add function to change backgrounds on mouseover

--- script.js
+++ script.js
@@ -9,10 +9,20 @@ for (let i=0; i<16; i++) {
 function createColumns(row) {
     for (let i=0; i<16; i++) { 
         let div = document.createElement('div');
-        div.classList.add('column');
+        div.classList.add('block');
         row.appendChild(div);
     }
 }
 
 const rows = document.querySelectorAll(".row");
 rows.forEach(createColumns);
+
+function changeBackground(square) {
+    square.setAttribute('id', "hovering")
+}
+
+const blocks = document.querySelectorAll(".block");
+blocks.forEach((block)=>{
+    block.addEventListener("mouseover", () => {changeBackground(block);
+    })})
+
