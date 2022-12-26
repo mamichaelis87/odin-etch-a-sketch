f1e4c61fce87c3beccaf4625865f4567b6781571
mamichaelis87 <mallenm1993@gmail.com>
2022-12-26 13:10:16 -0500

    Create 16 rows with styling, begin function for columns

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector(".container");
+
+for (let i=0; i<16; i++) { 
+    let div = document.createElement('div');
+    div.classList.add('row');
+    container.appendChild(div);
+}
+
+function createColumns(row) {
+    for (let i=0; i<16; i++) { 
+        let div = document.createElement('div');
+        div.classList.add('column');
+        row.appendChild(div);
+    }
+}
+
+const rows = document.querySelectorAll(".row");
+rows.forEach(createColumns)
